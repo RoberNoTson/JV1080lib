f3dc0f5c043a150b707bc18ffe91911228ed44b6
RoberNoTson <RoberNoTson@tx.rr.com>
2013-08-24 15:59:43 -0500

    updates

--- doc/insert_example.sql
+++
@@ -1,1 +0,0 @@
-NSERT INTO table1 VALUES(1, LOAD_FILE(data.png));
--- doc/load_tuning.sql
+++
@@ -1,9 +0,0 @@
-# load_tuning.sql
-
-replace into JV1080.Tuning () VALUES(DEFAULT,"Equal Temperament", x'404040404040404040404040', "Standard equal temperament tuning", DEFAULT);
-replace into JV1080.Tuning () VALUES(DEFAULT,"Well Tempered", x'464042443E463E444240463E', "Baroque-era Well Tempered tuning", DEFAULT);
-replace into JV1080.Tuning () VALUES(DEFAULT,"Pure Temperament", x'40384450323E36424E304E34', "JV-1080 suggested Pure Tempered tuning", DEFAULT);
-replace into JV1080.Tuning () VALUES(DEFAULT,"Just Temperament", x'404C4450323E2E4224305234', "Just quarter-comma Tempered tuning", DEFAULT);
-replace into JV1080.Tuning () VALUES(DEFAULT,"Pythagorean Temperament", x'4036443A483E4C4238463C4A', "Pythagorean natural Tempered tuning", DEFAULT);
-replace into JV1080.Tuning () VALUES(DEFAULT,"Meantone Temperament", x'4028394B32442B3D2436472E', "Meantone Tempered tuning", DEFAULT);
-replace into JV1080.Tuning () VALUES(DEFAULT,"Arabic Tuning", x'3A6D3E340D386B3C6F40360F', "Common pop-music tuning called Arabic", DEFAULT);
